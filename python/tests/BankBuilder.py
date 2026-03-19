292d38512c85431abcad21486212098a0f422b71
Maxime <maximesimorre@icoud.com>
2026-03-19 15:15:14 +0100

    bankbuilder

---
+++ python/tests/BankBuilder.py
@@ -0,0 +1,15 @@
+from xterm_craft_workshop.bank import Bank
+
+class BankBuilder:
+    def __init__(self):
+        self.rates = []
+
+    def with_rate(self, from_currency, to_currency, rate):
+        self.rates.append((from_currency, to_currency, rate))
+        return self
+
+    def build(self):
+        bank = Bank()
+        for from_currency, to_currency, rate in self.rates:
+            bank.addExchangeRate(from_currency, to_currency, rate)
+        return bank
